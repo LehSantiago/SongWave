1dc49a9ff0db95ec3950270de038612c5240c9a8
LehSantiago <101338974+LehSantiago@users.noreply.github.com>
2022-04-06 17:52:20 -0300

    Add files via upload

---
+++ Scripts/PlayerPageScripts/data.js
@@ -0,0 +1,58 @@
+const musics = [
+    {
+        title: 'XXI',
+        artist: 'Qr',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/107/xxi-1648548038-XKSYLmfyye.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/632db6f2-94a2-466a-8d33-68bd852441e4/artwork-440x440.jpg',
+    },
+    {
+        title: 'Whatever',
+        artist: 'Andromedik, Cartoon',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/106/whatever-1648206038-V4fcqmsmnB.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/fc7680fa-4265-4f09-bcdc-421f0c9bd0f9/artwork-440x440.jpg',
+    },
+    {
+        title: 'Odyssey',
+        artist: 'TOBSIK, Spyker, Kato',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/088/odyssey-1644584437-hQlykjKGb9.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/079bd53d-1d24-4f46-b8fa-4a336c281f30/artwork-440x440.jpg',
+    },
+    {
+        title: 'Lets Run Away',
+        artist: 'Rico 56, Doxed, Jay Eskar',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/090/lets-run-away-1645106436-UaUWe4Jkq9.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/bea85988-7b0d-4e2b-a4d9-cedf7df36de8/artwork-440x440.jpg',
+    },
+    {
+        title: 'Royalty',
+        artist: 'Maestro Chives, Egzod',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/936/royalty-1619082033-7RC2AlRdd1.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/1e14b2bc-be4b-440c-9060-1d840d0d72da/artwork-440x440.jpg',
+    },
+    {
+        title: 'CTRL Z',
+        artist: 'Halvorsen',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/703/ctrl-z-1586960609-EX9x5ddgWH.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/a5753b66-07c9-476c-9f9b-a79761cc3a48/artwork-440x440.jpg',
+    },
+    {
+        title: 'Numb The Pain',
+        artist: 'Shiah Maisel, Anikdote, CHENDA, Le Malls, Catas, Clarx',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/013/numb-the-pain-1632830435-Y79xc7Pkve.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/99ad2da2-13c9-40aa-b59d-54eea0886297/artwork-440x440.jpg',
+    },
+    {
+        title: 'Phoenix',
+        artist: 'Halvorsen, Netrum',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/997/phoenix-1629450035-ZSHfvWoObF.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/7438735b-7816-415a-b3bb-985b6a3f930b/artwork-440x440.jpg',
+    },
+    {
+        title: '21',
+        artist: 'Shiah Maisel, Arlow',
+        path: 'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/889/21-1610028032-ILqHvSooxR.mp3',
+        image: 'https://linkstorage.linkfire.com/medialinks/images/52a4a711-b81f-4a67-b0c8-aad2427ad127/artwork-440x440.jpg',
+    },
+]
+
+export { musics };
